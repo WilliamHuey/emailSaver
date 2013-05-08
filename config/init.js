af2b0c2731e2b2a118f3a4ba54bb8000cca16389
William Huey <wklhuey@gmail.com>
2013-05-08 13:51:22 -0700

    Add asset-smasher

--- config/init.js
+++ config/init.js
@@ -1,3 +1,25 @@
+var assetSmasher = require('asset-smasher');
+var Smasher = assetSmasher.Smasher;
+
+var sm = new Smasher({
+  paths: [__dirname + '/../public/js'],
+  only: ['application.js.mf'],
+  compress: false,
+  hash: true,
+  hashVersion: '1.0',
+  gzip: false,
+  outputTo: __dirname + '/../public/assets',
+  verbose: true,
+  noclean: false
+});
+
+sm.compileAssets(function(err) {
+  if(err) {
+    console.log('An error occurred', err);
+  } else {
+    console.log('Compilation done!');
+  }
+});
 
 // Add uncaught-exception handler in prod-like environments
 if (geddy.config.environment != 'development') {
